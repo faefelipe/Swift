57112546fa6f938f1d58bdeb6a7b92399017a916
Felipe Almeida <fae.20@hotmail.com>
2020-10-06 10:20:59 -0300

    Create button Start Task

--- ProgressView/ProgressView/ViewController.swift
+++ ProgressView/ProgressView/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setTitle("Start Task", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 9
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
