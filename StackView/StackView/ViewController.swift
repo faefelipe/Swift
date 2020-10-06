fbeb7e0358f33d1dc33dab686d3a7fade8d34f63
Felipe Almeida <fae.20@hotmail.com>
2020-10-06 09:18:23 -0300

    Add new label and remove position fixed with labels and imageView

--- StackView/StackView/ViewController.swift
+++ StackView/StackView/ViewController.swift
@@ -20,18 +20,27 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .systemGreen
         imageView.image = UIImage(systemName: "bell")
         imageView.contentMode = .scaleAspectFit
-        imageView.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: view.frame.size.height/3).isActive = true
         
         let label = UILabel()
+        label.backgroundColor = .systemBlue
         label.text = "Hello World"
         label.textAlignment = .center
         label.font = .systemFont(ofSize: 21, weight: .bold)
-        label.widthAnchor.constraint(equalToConstant: view.frame.size.width).isActive = true
-        label.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
-        let stackView = UIStackView(arrangedSubviews: [imageView, label])
+        let label2 = UILabel()
+        label2.backgroundColor = .systemPink
+        label2.text = "Another Label"
+        label2.textAlignment = .center
+        label2.font = .systemFont(ofSize: 21, weight: .bold)
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, label, label2])
         stackView.frame = view.bounds
+        stackView.backgroundColor = .systemYellow
+    
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        
         view.addSubview(stackView)
     }
 }
