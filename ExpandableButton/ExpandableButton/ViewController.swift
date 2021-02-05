fe8ad602c61d6180ffe493eddbb9e53f996a949a
Paco <paco@iPaddeDaniel2.localdomain>
2021-02-05 11:48:05 -0300

    Implemented Expandable Button

--- ExpandableButton/ExpandableButton/ViewController.swift
+++ ExpandableButton/ExpandableButton/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let buttonPanelView = ButtonPanelView()
+    private let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = "Tap!"
+        label.sizeToFit()
+        view.addSubview(label)
+        buttonPanelView.delegate = self
+        view.addSubview(buttonPanelView)
+        addConstraints()
     }
+    
+    private func addConstraints() {
+        buttonPanelView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonPanelView.bottomAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: buttonPanelView.bottomAnchor, constant: 35).isActive = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
 
-
+extension ViewController: ButtonPanelDelegate {
+    func didTapButtonWithText(_ text: String) {
+        guard text != label.text else { return }
+        label.text = text
+    }
 }
 
