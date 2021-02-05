2214977029e510a8a4e1a9db7ff9bb12ae397ae8
Felipe <fae.20@hotmail.com>
2021-02-05 18:01:10 -0300

    Initial project

---
+++ adaptive-layout/adaptive-layout/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  adaptive-layout
+//
+//  Created by Paco on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
